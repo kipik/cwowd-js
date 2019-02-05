93f96db7b45a72fc2b4262b474b4d468c9941b9c
kipik <kipik.kipik@gmail.com>
2019-02-05 18:25:25 +0100

    Ajout bouton Déconnexion (actif et redirection sur root)

--- cwowd/src/components/Header.vue
+++ cwowd/src/components/Header.vue
@@ -1,3 +1,4 @@
+/* eslint-disable */
 <template>
     <v-toolbar fixed class="cyan" dark>
         <v-toolbar-title class="mr-4">
@@ -15,12 +16,27 @@
                 flat dark to="login">
                 Connexion
             </v-btn>
+            <v-btn v-if="$store.state.isUserLoggedIn"
+                flat dark 
+                @click="logout">
+                Déconnexion
+            </v-btn>
         </v-toolbar-items>
     </v-toolbar>
 </template>
 
 <script>
-export default {}
+export default {
+    methods: {
+        logout () {
+            this.$store.dispatch('setToken', null)
+            this.$store.dispatch('setUser', null)
+            this.$router.push({
+                name: 'root'
+            })
+        }
+    }
+}
 </script>
 
 <style scoped>
